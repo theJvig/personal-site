cc7be001fbf492310e194ed0902167f78aa74713
Jacob Vig <jacobvig@Jacobs-MacBook-Pro.local>
2023-06-21 16:54:44 -0400

    Portfolio Site updaing packages, app, and contact section

--- src/Screens/Landing/Home/Contact.js
+++ src/Screens/Landing/Home/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContactForm from './ContactForm/ContactForm';
+import { ContactForm } from './ContactForm/ContactForm';
 
 const Contact = () => {
 
--- src/Screens/Landing/Home/ContactForm/ContactForm.js
+++ src/Screens/Landing/Home/ContactForm/ContactForm.js
@@ -1,43 +1,74 @@
-import React, {useState, ReactDom} from 'react';
+import React, {useState, useReducer} from 'react';
+import { googleFormsToJson } from 'react-google-forms-hooks';
 
-const ContactForm = () => {
 
+const formReducer = (state, event) => {
+    return{
+        ...state,
+        [event.name]: event.value
+    }
+}
+
+export const ContactForm = () => {
+    const [formData, setFormData] = useReducer(formReducer, {}); 
     const [firstName, lastName, email, shortAnser ] = useState(); 
+    const [submitting, setSubmitting] = useState(false);
 
     const handleSubmit = event => {
         event.preventDefault(); 
-        alert('The form was submitted');
+        setSubmitting(true);
+
+        setTimeout(() => {
+            setSubmitting(false);
+        }, 3000);
     };
+
+    const handleChange = event => {
+        setFormData({
+            name: event.target.name,
+            value: event.target.value
+        });
+    }
 
     return(
         <div className="contact-form-container">
+            {submitting && 
+                <div>
+                    You are submitting the following data: 
+                    <ul>
+                        {Object.entries(formData).map(([name, value]) => (
+                            <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+                        ))}
+                    </ul>
+                </div>
+            }
             <form onSubmit={handleSubmit}>
                 <label>First Name:
                     <input
                         type="text" 
-                        value={firstName}
-                        //onChange={(e) => setFirstName(e.target.value)}
+                        name="firstName"
+                        onChange={handleChange}
                     />
                 </label>
                 <label>Last Name:
                     <input
                         type="text"
-                        value={lastName}
-                        //onChange={(e) => setLastName(e.target.value)}
+                        name="lastName"
+                        onChange={handleChange}
                     />
                 </label>
                 <label>Email:
                     <input
                         type="text"
-                        value={email}
-                        //onChange={(e) => setEmail(e.target.value)}
+                        name="email"
+                        onChange={handleChange}
                     />
                 </label>
                 <label>How Can I help?
                     <input
                         type="textarea"
-                        value={shortAnser}
-                        //onChange={(e) => setShortAnser(e.target.value)}
+                        name="shortAnser"
+                        onChange={handleChange}
                     />
                 </label>
                 <button type="submit">Submit</button>
@@ -45,5 +76,3 @@ const ContactForm = () => {
         </div>
     )
 }
-
-export default ContactForm;
